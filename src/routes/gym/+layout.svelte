e311d0b5af8b1534c72816c4b6b68c808342f5a1
hansen1416 <badapplesweetie@gmail.com>
2024-06-01 16:21:15 +0800

    gym

--- src/routes/gym/+layout.svelte
+++ src/routes/gym/+layout.svelte
@@ -261,6 +261,90 @@
 			selectedAnimationKeyUnsubscribe();
 		}
 	});
+
+	/**
+ * 
+import { browser } from "$app/environment";
+import WebSocketClient from "../lib/WebSocketClient";
+import WebSocketClient from "../lib/WebSocketClient";
+import type { AnimationQueueItem } from "../types/index";
+import {
+	animationDictStore,
+	animationQueueStore,
+	websocketStateStore,
+} from "../store/store";
+
+let socket: WebSocketClient;
+
+onMount(() => {
+	socket = new WebSocketClient();
+	// When you write $websocket, you're essentially saying, "Get the current value from the store named websocket."
+
+	// websocket.set(socket); // Assign the instance to the store
+	// Connect to the websocket
+	socket.connect("ws://localhost:3333/ws");
+
+	socket.onConnect = (ws_state) => {
+		websocketStateStore.set(ws_state);
+	};
+
+	socket.onMessage = (msg) => {
+		// check if msg is an ArrayBuffer(0)
+		if (msg instanceof ArrayBuffer && msg.byteLength === 0) {
+			console.log("received heartbeat message");
+			return;
+		}
+
+		if (typeof msg !== "string") {
+			console.log(
+				"websocket message received non-string message",
+				msg,
+			);
+			return;
+		}
+
+		// first split the message
+		const [redis_key, data] = (msg as string).split("::");
+
+		const [category, name] = redis_key.split(":");
+
+		if (category === "am") {
+			// 'am' is animation data for a single animation
+			console.log("received animation data", name);
+
+			animationDictStore.update((old_data) => {
+				return { ...old_data, [name]: data };
+			});
+		} else if (category === "amq") {
+			// 'amq' is animation queue, a list of animation metadata
+
+			const anim_quque = JSON.parse(data);
+
+			console.log("received animation queue", anim_quque);
+
+			// update animation_queue
+			animationQueueStore.update((old_queue) => {
+				return [...old_queue, ...anim_quque];
+			});
+
+			// iterate through animation_queue, and request animation data
+			anim_quque.forEach((anim: AnimationQueueItem) => {
+				const msg = "am:" + anim.name;
+
+				socket.sendMessage(msg);
+			});
+		} else {
+			console.log("received unknown message", msg);
+		}
+	};
+});
+
+onDestroy(() => {
+	if (browser) {
+		socket.close();
+	}
+});
+*/
 </script>
 
 <!-- section is not needed, only for readablity -->
